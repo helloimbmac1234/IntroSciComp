a8954481284000bed84cc54b0fc0e72ec713fae2
Blake MacNelly <Blake MacNelly>
2016-10-05 14:38:24 -0500

    Renamed csv as 'dna'. Added GC_Content function that calculates gc content regardless of capitalization of letters in sequence, then included the function applied to the dna file.

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,2 +1,10 @@
 #Repairing Code for House_Elf Analysis
-read.csv("house_elf_dna_data_1.csv")
+dna <- read.csv("house_elf_dna_data_1.csv")
+#Problem 6
+GC_Content <- function(sequence){
+  str_to_lower(sequence)
+  Gs <- str_count(sequence, "G")
+  Cs <- str_count(sequence, "C")
+  gc_content <- (Gs + Cs) / str_length(sequence) * 100 
+  return(gc_content)}
+GC_Content(dna$dnaseq)
